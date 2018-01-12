da64271c52278622be53a5b61b1a3b4ab970d241
jmcpherson9@mail.greenriver.edu <jmcphers@srv603.webhostingforstudents.com>
2018-01-12 14:39:05 -0500

    added page1 route

--- index.php
+++ index.php
@@ -10,10 +10,16 @@ require "vendor/autoload.php";
 
 $f3 = Base::instance();
 
+//set debug level
+$f3->set('DEBUG',3);
+
+$f3->route("GET /page1", function(){
+    echo "<h1>Routing demo and more!!</h1>";
+});
+
 $f3->route("GET /", function(){
     echo "<h1>Routing demo!</h1>";
 });
-
 $f3->run();
 
 ?>
