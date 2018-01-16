01910542797ff909e6c72f013a51b5d98267a1c1
jmcpherson9@mail.greenriver.edu <jmcphers@srv603.webhostingforstudents.com>
2018-01-16 14:10:56 -0500

    added routes demonstrated in class

--- index.php
+++ index.php
@@ -13,13 +13,35 @@ $f3 = Base::instance();
 //set debug level
 $f3->set('DEBUG',3);
 
-$f3->route("GET /page1", function(){
-    echo "<h1>Routing demo and more!!</h1>";
+$f3->route("GET /hello", function(){
+    echo "<h1>hello</h1>";
 });
 
 $f3->route("GET /", function(){
     echo "<h1>Routing demo!</h1>";
 });
+
+$f3->route("GET /page2", function (){
+    echo "<h1>YOU THOUGHT THIS WAS PAGE 2, BUT IT WAS ME! DIO!</h1>";
+});
+
+$f3->route('GET /language/@lang', function(){
+    function($f3, $params){
+        switch ($params['lang']){
+            case 'swahili':
+                echo "jumbo!"; break;
+            default:
+                $f3->error(404);
+        }
+    }
+});
+
+$f3->set('name', $params['name'], function($f3, $params){
+   $f3->set('name', $params['name']);
+   $template = new Template();
+   echo $template->render('views/hello.html');
+});
+
 $f3->run();
 
 ?>
